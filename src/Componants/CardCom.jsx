3516cee7c96552adad4b1b037089d9842341b579
mangalsingh123t <121483110+mangalsingh123t@users.noreply.github.com>
2024-07-29 11:32:36 +0530

    Implement slice method

--- src/Componants/CardCom.jsx
+++ src/Componants/CardCom.jsx
@@ -16,30 +16,30 @@ export default function CardCom() {
 
     return (
         <>
-         {data && (
-    <div className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-5 ">
-            {data.map((item, index) => (
-                <div key={index} className="p-2">
-                    <div className="card bg-white shadow-lg rounded-lg">
-                        <div className="p-4">
-                            <h5 className="text-lg font-bold">{item.title}</h5>
-                            <p className="text-sm text-gray-600">{item.description.slice(0, 150)}</p>
-                            <div className='text-center'>
-                            <button
-                                className="btn btn-primary mt-3 bg-blue-500 text-white px-4 py-2 rounded"
-                                onClick={() => dispatch(incremenCardItemCount(item.id))}
-                            >
-                                ADD TO CART
-                            </button>
+            {data && (
+                <div className="container mx-auto">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-5 ">
+                        {data.map((item, index) => (
+                            <div key={index} className="p-2">
+                                <div className="card bg-white shadow-lg rounded-lg">
+                                    <div className="p-4">
+                                        <h5 className="text-lg font-bold">{item.title.slice(0, 10)}</h5>
+                                        <p className="text-sm text-gray-600">{item.description.slice(0, 150)}</p>
+                                        <div className='text-center'>
+                                            <button
+                                                className="btn btn-primary mt-3 bg-blue-500 text-white px-4 py-2 rounded"
+                                                onClick={() => dispatch(incremenCardItemCount())}
+                                            >
+                                                ADD TO CART
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
-            ))}
-        </div>
-    </div>
-)}
+            )}
 
         </>
     );
